013c09a09a52c1211465c8a2872f1b0f75c5c33c
mushfiqbh <mushfiqbh@gmail.com>
2026-05-20 00:59:44 +0600

    Laravel Blade Project Setup Initial

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+    base: "/",
+    build: {
+        outDir: "public/build",
+        manifest: true,
+        assetsDir: "assets",
+    },
+    plugins: [
+        laravel({
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+});
