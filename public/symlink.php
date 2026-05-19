9bf2199e79c6b39adbaac276d61869c83f90f957
mushfiqbh <mushfiqbh@gmail.com>
2026-05-20 01:01:05 +0600

    Database Migration Implementation Done

---
+++ public/symlink.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Create symlinks for Barnomala Client.
+ * Run this once, then delete for security.
+ */
+
+$parentDir = dirname(__DIR__);
+$target = $parentDir . '/laravel_projects/landing_site/storage/app/public';
+$link = $parentDir . '/public_html/storage';
+
+if (file_exists($link)) {
+    echo "⚠️ Skipping: Storage Symlink Already Exists.";
+} else if (symlink($target, $link)) {
+    echo "✅ Storage Symlink Successfully Created.";
+} else {
+    echo "❌ Failed to Create Storage Symlink.";
+}
